f00fbaa6cc481a64fd2a414060ec5f737bec9fab
Ray725@users.noreply.github.com <>
2019-02-04 18:07:41 -0500

    Make 200 check in App.js stricter

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
     const response = await fetch('/api/hello');
     const body = await response.json();
 
-    if (response.status != 200) throw Error(body.message);
+    if (response.status !== 200) throw Error(body.message);
 
     return body;
   };

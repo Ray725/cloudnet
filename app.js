2a79c25a17502d6c75f03bd6bc39f9ed371a1996
Ray725@users.noreply.github.com <>
2019-02-04 17:43:46 -0500

    Test jenkins with mocha

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const globalvar = 0;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -9,8 +10,18 @@ app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
 });
 
+app.post('/api/modifyVar', (req, res) => {
+  globalvar = req.body.change;
+  res.send(
+    `Modified global var to be ${globalvar}`
+  );
+});
+
+app.get('/api/getVar', (req, res) => {
+  res.send(`globalvar: ${globalvar}`);
+});
+
 app.post('/api/world', (req, res) => {
-  console.log(req.body);
   res.send(
     `I received your POST request. This is what you sent me: ${req.body.post}`,
   );
